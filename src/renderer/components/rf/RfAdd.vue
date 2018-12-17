065a40750e5ad6b58750a9382ecdad89484f55a4
Nik <trnklvariant@gmail.com>
2018-12-17 17:39:34 +0300

    Update RfAdd.vue

--- src/renderer/components/rf/RfAdd.vue
+++ src/renderer/components/rf/RfAdd.vue
@@ -8,7 +8,7 @@
         <v-form ref="form" v-model="valid" validation>
           <v-container grid-list-md>
             <v-layout wrap>
-              <v-flex xs12>
+              <v-flex xs7 md7 lg7>
                 <v-text-field
                     prepend-icon="list"
                     name="number"
@@ -19,6 +19,33 @@
                     required
                     :rules="numberRules"
                 ></v-text-field>
+              </v-flex>
+              <v-flex xs5 md5 lg5>
+                <v-menu
+                    :close-on-content-click="false"
+                    v-model="menuDate"
+                    :nudge-right="40"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    max-width="290px"
+                    min-width="290px"
+                >
+                  <v-text-field
+                      slot="activator"
+                      v-model="date"
+                      name="date"
+                      label="Date"
+                      hint="MM/DD/YYYY"
+                      persistent-hint
+                      prepend-icon="event"
+                      readonly
+                  ></v-text-field>
+                  <v-date-picker v-model="dateP" no-title
+                                 @input="menuDate = false"
+                  ></v-date-picker>
+                </v-menu>
               </v-flex>
             </v-layout>
           </v-container>
@@ -55,7 +82,7 @@
 
 <script>
   export default {
-    data: () => ({
+    data: vm => ({
       number: '',
       date: '',
       buyer: '',
@@ -65,7 +92,10 @@
         // нужно еще ограничить ввод чисто цифрами
         v => !!v || 'Number is required',
         v => (v && v.length === 3) || 'Number must have 3 characters'
-      ]
+      ],
+      menuDate: false,
+      dateP: new Date().toISOString().substr(0, 10),
+      dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10))
     }),
     computed: {
       loading () {
@@ -73,6 +103,14 @@
       },
       addDialog () {
         return this.$store.getters.getAddDialog
+      },
+      computedDateFormatted () {
+        this.date = this.formatDate(this.dateP)
+      }
+    },
+    watch: {
+      date (val) {
+        this.dateFormatted = this.formatDate(this.dateP)
       }
     },
     methods: {
@@ -96,7 +134,18 @@
       switchAddDialog () {
         this.$store.dispatch('changeAddDialog')
       },
-      addNewDoc () {}
+      formatDate (date) {
+        if (!date) return null
+
+        const [year, month, day] = date.split('-')
+        return `${month}/${day}/${year}`
+      },
+      parseDate (date) {
+        if (!date) return null
+
+        const [month, day, year] = date.split('/')
+        return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+      }
     }
   }
 </script>
